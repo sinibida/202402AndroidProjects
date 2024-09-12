86df313dc3a72bb6475dfb57e1e120a371cf765e
SPAUPA <sinibida@gmail.com>
2024-09-12 10:40:47 +0900

    feat: buttons & onButton1Click

--- Hello/app/src/main/java/com/techtown/hello/MainActivity.java
+++ Hello/app/src/main/java/com/techtown/hello/MainActivity.java
@@ -1,6 +1,8 @@
 package com.techtown.hello;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main); // 액티비티 표시
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onButton1Click(View view) {
+        Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
     }
 }
